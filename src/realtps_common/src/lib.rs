b29fe04a19a5cd5f95714368fb44893eb6f832fc
aimeedeer <aimeez@pm.me>
2021-12-11 17:26:18 -0800

    setup DB

--- src/realtps_common/src/lib.rs
+++ src/realtps_common/src/lib.rs
@@ -1,3 +1,71 @@
+use serde::{Serialize, Deserialize};
+use anyhow::Result;
+use std::fs::{self, File};
+use std::io::BufWriter;
+use std::fmt;
+    
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Chain {
+    Ethereum,
+    Polygon,    
+}
+
+impl fmt::Display for Chain {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Chain::Ethereum => write!(f, "ethereum"),
+            Chain::Polygon => write!(f, "polygon"),
+        }
+    }
+}
+    
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Block {
+    pub chain: Chain,
+    pub block_number: u64,
+    pub timestamp: u64, // seconds since unix epoch
+    pub num_txs: u64,
+    pub hash: String,
+    pub parent_hash: String,
+}
+
+pub trait Db {
+    fn store_block(&self, block: Block) -> Result<()>;
+    fn load_block(&self, block_number: u64) -> Result<Option<Block>>;
+    fn get_latest_block_number(&self, chain: Chain) -> Result<u64>;
+}
+
+pub struct JsonDb;
+
+pub static JSON_DB_DIR: &'static str = "db/";
+
+impl Db for JsonDb {
+    fn store_block(&self, block: Block) -> Result<()> {
+        fs::create_dir_all(JSON_DB_DIR)?;
+
+        let path = format!("{}/{}/{}", JSON_DB_DIR, block.chain, block.block_number);
+        println!("writing to file {}", path);
+        
+        let file = File::create(path)?;
+        let mut writer = BufWriter::new(file);
+
+        serde_json::to_writer(&mut writer, &block)?;
+
+        println!("file saved");
+        Ok(())
+    }
+
+    fn load_block(&self, block_number: u64) -> Result<Option<Block>> {
+        todo!()
+    }
+
+    fn get_latest_block_number(&self, chain: Chain) -> Result<u64> {
+        todo!()
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/realtps_import/src/main.rs
+++ src/realtps_import/src/main.rs
@@ -1,5 +1,6 @@
 use structopt::StructOpt;
 use anyhow::Result;
+use realtps_common::{Block, Chain, JsonDb, Db};
 
 #[derive(StructOpt, Debug)]
 struct Opt {
@@ -9,12 +10,24 @@ struct Opt {
 
 #[derive(StructOpt, Debug)]
 enum Command {
-    ReadBlock { number: u32 },
+    ReadBlock { number: u64 },
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
 
     println!("opt: {:?}", &opt);
+
+    let test_block = Block {
+        chain: Chain::Ethereum,
+        block_number: 123,
+        timestamp: 333, 
+        num_txs: 222,
+        hash: "hash".to_string(),
+        parent_hash: "parent_hash".to_string(),
+    };
+    let json_db = Box::new(JsonDb);
+    
+    json_db.store_block(test_block)?;
     Ok(())
 }
