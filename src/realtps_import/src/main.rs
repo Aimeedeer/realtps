76824c0ccd361ec39222562e485c0a34ced2a4f2
Brian Anderson <andersrb@gmail.com>
2021-12-12 20:27:29 -0800

    .

--- src/realtps_import/src/main.rs
+++ src/realtps_import/src/main.rs
@@ -328,7 +328,10 @@ impl Importer {
     }
 }
 
-struct ChainCalcs;
+struct ChainCalcs {
+    chain: Chain,
+    tps: f64,
+}
 
 async fn calculate_for_chain(db: Arc<Box<dyn Db>>, chain: Chain) -> Result<ChainCalcs> {
     let highest_block_number = {
@@ -355,10 +358,41 @@ async fn calculate_for_chain(db: Arc<Box<dyn Db>>, chain: Chain) -> Result<Chain
     let seconds_per_week = 60 * 60 * 24 * 7;
     let min_timestamp = latest_timestamp.checked_sub(seconds_per_week).expect("underflow");
 
-    loop {
-    }
+    let mut current_block_number = highest_block_number;
+    let mut current_block = load_block(current_block_number).await?;
 
-    todo!()
+    let mut num_txs: u64 = 0;
+
+    let init_timestamp = loop {
+        num_txs = num_txs.checked_add(current_block.num_txs).expect("overflow");
+
+        assert!(current_block_number != 0);
+
+        let prev_block_number = current_block_number - 1;
+        let prev_block = load_block(prev_block_number).await?;
+
+        assert!(prev_block.timestamp <= current_block.timestamp);
+
+        if prev_block.timestamp <= min_timestamp {
+            break prev_block.timestamp;
+        }
+        if prev_block.block_number == 0 {
+            break prev_block.timestamp;
+        }
+    };
+
+    assert!(init_timestamp <= latest_timestamp);
+    let total_seconds = latest_timestamp - init_timestamp;
+    let total_seconds_u32 = u32::try_from(total_seconds).map_err(|_| anyhow!("seconds overflows u32"))?;
+    let num_txs_u32 = u32::try_from(num_txs).map_err(|_| anyhow!("num txs overflows u32"))?;
+    let total_seconds_f64 = f64::from(total_seconds_u32);
+    let num_txs_f64 = f64::from(num_txs_u32);
+    let tps = num_txs_f64 / total_seconds_f64;
+
+    Ok(ChainCalcs {
+        chain,
+        tps,
+    })
 }
 
 fn ethers_block_to_block(chain: Chain, block: ethers::prelude::Block<H256>) -> Result<Block> {
