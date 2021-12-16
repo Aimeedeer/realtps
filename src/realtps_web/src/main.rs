a2e7ead49ebba51463c5b91aae67390cc08a9d87
aimeedeer <aimeez@pm.me>
2021-12-15 17:43:29 -0800

    add rocket templates

--- src/realtps_web/src/main.rs
+++ src/realtps_web/src/main.rs
@@ -1,12 +1,23 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_dyn_templates::Template;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Context {
+    
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, Real TPS!"
+fn index() -> Template {
+    let context = Context {};
+    Template::render("index", &context)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
