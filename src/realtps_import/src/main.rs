d28aa107128afad2cc0051591eb267d83af44e28
aimeedeer <aimeez@pm.me>
2021-12-17 11:03:33 -0800

    .

--- src/realtps_import/src/main.rs
+++ src/realtps_import/src/main.rs
@@ -80,7 +80,7 @@ async fn run(cmd: Command, rpc_config: RpcConfig) -> Result<()> {
 }
 
 fn load_rpc_config<P: AsRef<Path>>(path: P) -> Result<RpcConfig> {
-    let rpc_config_file = fs::read_to_string(path).context("unable to load RPC config")?;
+    let rpc_config_file = fs::read_to_string(path).context("unable to load RPC configuration")?;
     let rpc_config = toml::from_str::<RpcConfig>(&rpc_config_file)
         .context("unable to parse RPC configuration")?;
 
