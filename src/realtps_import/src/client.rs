ed497faab580f312335ededdf46d571cac159007
Brian Anderson <andersrb@gmail.com>
2021-12-22 12:37:01 -0800

    Filter out solana vote txs

--- src/realtps_import/src/client.rs
+++ src/realtps_import/src/client.rs
@@ -14,6 +14,7 @@ use solana_transaction_status::UiTransactionEncoding;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::task;
+use log::{debug, trace};
 
 #[async_trait]
 pub trait Client: Send + Sync + 'static {
@@ -183,6 +184,42 @@ fn solana_block_to_block(
     block: solana_transaction_status::EncodedConfirmedBlock,
     slot_number: u64,
 ) -> Result<Block> {
+    fn calc_user_txs(block: &solana_transaction_status::EncodedConfirmedBlock) -> u64 {
+        let mut num_user_txs = 0;
+        for tx_status in &block.transactions {
+            let tx = tx_status.transaction.decode().unwrap();
+            trace!("tx_meta: {:#?}", tx_status.meta.as_ref().unwrap());
+            trace!("tx: {:#?}", tx);
+            let account_keys = &tx.message.account_keys;
+            let mut num_vote_instrs = 0;
+            for instr in &tx.message.instructions {
+                let program_id_index = instr.program_id_index;
+                let program_id = account_keys[usize::from(program_id_index)];
+
+                if program_id == solana_sdk::vote::program::id() {
+                    num_vote_instrs += 1;
+                    trace!("found vote instruction");
+                } else {
+                    trace!("non-vote instruction");
+                }
+            }
+            if num_vote_instrs == tx.message.instructions.len() {
+                trace!("it's a vote transaction");
+            } else {
+                // This doesn't look like a vote transaction
+                trace!("it's a non-vote transaction");
+                num_user_txs += 1;
+            }
+        }
+
+        let vote_txs = block.transactions.len().checked_sub(num_user_txs).expect("underflow");
+        debug!("solana total txs: {}", block.transactions.len());
+        debug!("solana user txs: {}", num_user_txs);
+        debug!("solana vote txs: {}", vote_txs);
+
+        u64::try_from(num_user_txs).expect("u64")
+    }
+
     Ok(Block {
         chain: Chain::Solana,
         block_number: slot_number,
@@ -192,7 +229,7 @@ fn solana_block_to_block(
                 .block_time
                 .ok_or_else(|| anyhow!("block time unavailable for solana slot {}", slot_number))?,
         )?,
-        num_txs: u64::try_from(block.transactions.len())?,
+        num_txs: calc_user_txs(&block),
         hash: block.blockhash,
         parent_hash: block.previous_blockhash,
     })
