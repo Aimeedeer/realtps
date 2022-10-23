1e3ab8aacf7e874c6e446ab68157511ee45073de
Brian Anderson <andersrb@gmail.com>
2022-10-23 22:03:46 +0000

    optimism

--- src/realtps_import/src/delay.rs
+++ src/realtps_import/src/delay.rs
@@ -21,7 +21,7 @@ pub fn block_pace(chain: Chain) -> u64 {
     let msecs = match chain {
         Chain::Arbitrum => 400, // Subsecond block time
         Chain::Elrond => 1000, // 6s block time
-        Chain::Optimism => 1000, // Got blocked at 900ms, unclear what rate they want
+        Chain::Optimism => 2000, // Got blocked at 1000ms, unclear what rate they want
         // Need to go fast to keep up.
         // Solana's RpcClient will use its built in rate limiter when connecting to public nodes.
         Chain::Solana => 0,
